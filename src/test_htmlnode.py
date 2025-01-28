bfdb553b4b3d1d1d1ca5f8adf926e3daa44caed0
3lackC4t <CScottDev01@proton.me>
2025-01-27 21:25:04 -0500

    Unit testing for the ParentNode Class

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,26 +1,31 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
-class TestTextNode(unittest.TestCase):
+class TestHTMLNode(unittest.TestCase):
     def test_eq(self):
-        child_node = HTMLNode()
         node1 = HTMLNode(
-            tag="<p>", value="test", children=[child_node], props={"src": "script.js"}
+            tag="<p>",
+            value="test",
+            children=[HTMLNode(tag="p")],
+            props={"src": "script.js"},
         )
         node2 = HTMLNode(
-            tag="<p>", value="test", children=[child_node], props={"src": "script.js"}
+            tag="<p>",
+            value="test",
+            children=[HTMLNode(tag="p")],
+            props={"src": "script.js"},
         )
         self.assertEqual(node1, node2)
 
     def test_neq(self):
-        node1 = HTMLNode(props={"href": "styles.css"})
+        node1 = HTMLNode(tag="", props={"href": "styles.css"})
         node2 = HTMLNode(tag="<p>")
         self.assertNotEqual(node1, node2)
 
     def test_props_to_html(self):
-        node = HTMLNode()
+        node = HTMLNode(tag="<p>")
         properties = {
             "src": "script.js",
             "href": "styles.css",
@@ -45,6 +50,29 @@ class TestTextNode(unittest.TestCase):
 
         result_str = '<h1 style="color: red; font_weight: heavy;" required class="leaf-test" id="leaf-id">The Final Heading</h1>'
         self.assertEqual(node.to_html(), result_str)
+
+    def test_recursive_parent_to_html(self):
+        node = ParentNode(
+            tag="div",
+            children=[
+                LeafNode(tag="h1", value="A List"),
+                ParentNode(
+                    tag="ul",
+                    children=[
+                        LeafNode(tag="li", value="Item 1"),
+                        LeafNode(tag="li", value="Item 2"),
+                        LeafNode(tag="li", value="Item 3"),
+                    ],
+                ),
+            ],
+        )
+
+        result = "<div><h1>A List</h1><ul><li>Item 1</li><li>Item 2</li><li>Item 3</li></ul></div>"
+        self.assertEqual(node.to_html(), result)
+
+    def test_empty_parent_node(self):
+        node = ParentNode(tag="p", children=[LeafNode(tag="p")])
+        self.assertEqual(node.to_html(), "<p><p></p></p>")
 
 
 if __name__ == "__main__":
